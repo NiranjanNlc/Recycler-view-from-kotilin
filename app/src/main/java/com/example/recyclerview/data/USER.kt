d912b4ecfc1fd7b925ecb3fbd3eb3de8e5fee8ea
niranjannlc <niranjannlc10@gmail.com>
2022-06-13 06:36:43 +0545

    addig some data to the enum class

--- app/src/main/java/com/example/recyclerview/data/USER.kt
+++ app/src/main/java/com/example/recyclerview/data/USER.kt
@@ -2,8 +2,12 @@ package com.example.recyclerview.data
 
 enum class USER (val nam:String,val addr:String)
 {
-    NLC("Niranjan","Nijgaadh"),
+    NLC("Niranjan lamichhane","Nijgaadh"),
     AB("AAshma baniya","Hetauda"),
     SB("Shiwani Bista","Kathmandu"),
-    RP("Rashmi parde","Dang")
+    RP("Rashmi parde","Dang"),
+    AS("Ashmita Shrestha  ","Kathmandu "),
+    SU("Sushmita Bastola ","Solukhumbu "),
+    SS("Sushma Syangthan ","Lalitpur"),
+    NS("Namita Sahi ","Bhaktapur ")
 }

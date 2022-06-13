b5bf85e2c22ce3f080117e360bf57981e989d0a0
niranjannlc <niranjannlc10@gmail.com>
2022-06-13 06:47:22 +0545

    adding for te screen shots

--- app/src/main/java/com/example/recyclerview/data/USER.kt
+++ app/src/main/java/com/example/recyclerview/data/USER.kt
@@ -3,7 +3,7 @@ package com.example.recyclerview.data
 enum class USER (val nam:String,val addr:String)
 {
     NLC("Niranjan lamichhane","Nijgaadh"),
-    AB("AAshma baniya","Hetauda"),
+    AB("Aashma baniya","Hetauda"),
     SB("Shiwani Bista","Kathmandu"),
     RP("Rashmi parde","Dang"),
     AS("Ashmita Shrestha  ","Kathmandu "),
